260b40258a72120219ad4e22c9703916c7d07602
bbangho <dudgh614@gmail.com>
2024-09-10 13:25:16 +0900

    step 1-4 Multi thread
    multi thread는 싱글 코어 멀티쓰레딩에선 불리. Threa끼린 Heap을 공유하기 때문에 엉뚱한 값을 읽어오거나 수정할 수 있다.

--- src/main/java/com/nhnacademy/App.java
+++ src/main/java/com/nhnacademy/App.java
@@ -18,22 +18,18 @@ public class App
 {
     public static void main( String[] args )
     {
-        //TODO#1 CounterHandler 객체를 생성 합니다. countMaxSize : 10
-        CounterHandler counterHandler;
+        CounterHandler counterHandler = new CounterHandler(10);
 
-        //TODO#2 threadA 생성시 counterHandler 객체를 paramter로 전달 합니다.
-        Thread threadA;
+        Thread threadA = new Thread(counterHandler);
 
-        //TODO#3 threadA의 name을 'my-counter-A' 로 설정 합니다.
+        threadA.setName("my-counter-A");
+        Thread threadB = new Thread(counterHandler);
 
-        //TODO#4 threadB 생성시 counterHandler 객체를 paramter로 전달 합니다.
-        Thread threadB;
-
-        //TODO#5 threadB의 name을 'my-counter-B' 로 설정 합니다.
-
-        //TODO#7 threadA를 시작 합니다.
-
-        //TODO#8 threadB를 시작 합니다.
-
+        threadB.setName("my-counter-B");
+        threadA.start();
+        threadB.start();
+        // 생기는 문제 같은 변수를 읽으니 1 이 두번 나올수도 있고
+        // 우리가 의도한건 count 가 10이하일 동안 동작하는것인데
+        // while문 안에 두번읽어서 11이 나올수 있다. 
     }
 }
